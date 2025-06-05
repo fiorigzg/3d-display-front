89c40defb8598c7218a34b7b583d07508d0070d5
fiorigzg <fiorigzg@gmail.com>
2025-06-05 21:14:24 +0300

    feat: new shelves domain

--- constants/main.js
+++ constants/main.js
@@ -1,5 +1,5 @@
 export const serverUrl = "https://3d-display-back.sergei-kozlov-sites.ru";
 export const localUrl = "https://3ddisplay.sergei-kozlov-sites.ru";
-export const shelfUrl = "https://3ddispshelflay.sergei-kozlov-sites.ru";
+export const shelfUrl = "https://3d-display-shelves.sergei-kozlov-sites.ru";
 export const isIdInTable = true;
 export const reloadOnSave = true;
