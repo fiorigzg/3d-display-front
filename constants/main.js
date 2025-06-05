fcc6272d8f6bcaab02ce2a3628dd46b2806b092c
fiorigzg <fiorigzg@gmail.com>
2025-06-05 21:42:45 +0300

    fix: domain again

--- constants/main.js
+++ constants/main.js
@@ -1,4 +1,4 @@
-export const serverUrl = "https://back.3d-dpl.ru";
+export const serverUrl = "https://3d-display-back.sergei-kozlov-sites.ru";
 export const localUrl = "https://3ddisplay.sergei-kozlov-sites.ru";
 export const shelfUrl = "https://3d-display-shelves.sergei-kozlov-sites.ru";
 export const isIdInTable = true;
